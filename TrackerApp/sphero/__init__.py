6eae640120e2a7fdce081d01ce6e52e5c514c5da
Simon Nistad <simon.nistad@hotmail.com>
2014-04-10 16:59:16 +0200

    Created a sphero util file - Added support for transforming sphero coordinates to host coordinates and visa versa

--- TrackerApp/sphero/__init__.py
+++ TrackerApp/sphero/__init__.py
@@ -2,3 +2,4 @@ from core import SpheroAPI, SpheroError
 from constants import *
 from manager import SpheroManager
 from streaming import SensorStreamingConfig, SensorStreamingResponse
+from util import device_to_host_angle, host_to_device_angle
